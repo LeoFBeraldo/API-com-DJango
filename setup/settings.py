a7762dfe317a198d6fab916217fd9893fe3471f8
LeoFBeraldo <leonardo.fberaldo@gmail.com>
2022-08-31 18:30:50 -0300

    ajudatando gitignore

--- setup/settings.py
+++ setup/settings.py
@@ -19,8 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jr-o^i@@!aob@ods6+z=o6ix2f0ai3ye^1sfxpfgyb_nbun)t%'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
